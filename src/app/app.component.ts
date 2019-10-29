bdbca003d42ba3e1f098efd923cf552489308923
Leatia Stevenson <leatia09@icloud.com>
2019-10-29 12:09:50 -0500

    part 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,13 +10,38 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
-    this.sourceList = [
-       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.sourceList = [];
+    this.displayList = [];
+    let satellitesURL = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesURL).then(function(response) {
+      response.json().then(function(data) {
+
+        let fetchedSatellites = data.satellites;
+        
+        for (let i = 0; i < fetchedSatellites.length; i++) {
+          let spaceObject = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          this.sourceList.push(spaceObject);
+        }
+        this.displayList = this.sourceList.slice(0);
+      }.bind(this));
+    }.bind(this));
+  }
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    this.displayList = matchingSatellites;
  }
-}
+ }
+
+
+
+
